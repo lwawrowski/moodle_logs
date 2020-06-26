0fe62b94d4561c8a4b5f1983b14ee237b6dc1000
lwawrowski <lukasz8989@gmail.com>
2020-06-26 12:20:12 +0200

    wykresy

--- 02_analiza_czestosci.R
+++ 02_analiza_czestosci.R
@@ -1,17 +1,64 @@
 library(tidyverse)
+library(ggrepel)
 
 load("data/logs_anonim.RData")
 
 # skladnik
 
 logs_anonim %>% 
+  filter(nazwa_anonim != "ŁW") %>% 
   count(skladnik) %>% 
   arrange(n) %>% 
   filter(n > 1000) %>% 
   mutate(skladnik=fct_reorder(skladnik,n)) %>% 
   ggplot(aes(x=skladnik, y=n)) + 
-  geom_col() + 
-  coord_flip()
+  geom_col(fill = "#6daaee") + 
+  geom_text(aes(label = n), hjust = 1.1) +
+  xlab("Liczba wyświetleń") +
+  ylab("Typ składnika kursu") +
+  coord_flip() +
+  ggtitle("Typ składnika kursu wg liczby wyświetleń - powyżej 1000") +
+  labs(caption = "Łukasz Wawrowski - wawrowski.edu.pl") +
+  theme_light() +
+  theme(plot.caption = element_text(color = "grey80"))
+
+# zasób
+
+logs_anonim %>% 
+  filter(kontekst_zdarzenia != "Kurs: Statystyka - stacj. I st. - RiFP - (lab.) - dr Łukasz Wawrowski") %>% 
+  count(kontekst_zdarzenia) %>% 
+  mutate(kontekst_zdarzenia=gsub(" - quiz", "", kontekst_zdarzenia)) %>% 
+  filter(n > 1000) %>% 
+  mutate(kontekst_zdarzenia=fct_reorder(kontekst_zdarzenia,n)) %>% 
+  ggplot(aes(x=kontekst_zdarzenia, y=n)) + 
+  geom_col(fill = "#6daaee") + 
+  geom_text(aes(label = n), hjust = 1.1) +
+  xlab("Liczba wyświetleń") +
+  ylab("Składnik kursu") +
+  coord_flip() +
+  ggtitle("Składniki kursu wg liczby wyświetleń - powyżej 1000") +
+  labs(caption = "Łukasz Wawrowski - wawrowski.edu.pl") +
+  theme_light() +
+  theme(plot.caption = element_text(color = "grey80"))
+
+# adresy URL
+
+logs_anonim %>% 
+  filter(grepl("Adres URL:", kontekst_zdarzenia)) %>% 
+  mutate(kontekst_zdarzenia=gsub("Adres URL: ", "", kontekst_zdarzenia)) %>% 
+  count(kontekst_zdarzenia) %>% 
+  mutate(kontekst_zdarzenia=fct_reorder(kontekst_zdarzenia,n)) %>% 
+  ggplot(aes(x=kontekst_zdarzenia, y=n)) + 
+  geom_col(fill = "#6daaee") + 
+  geom_text(aes(label = n), hjust = 1.1) +
+  xlab("Liczba wyświetleń") +
+  ylab("Składnik kursu") +
+  coord_flip() +
+  ggtitle("Adresy URL wg liczby wyświetleń") +
+  labs(caption = "Łukasz Wawrowski - wawrowski.edu.pl") +
+  theme_light() +
+  theme(plot.caption = element_text(color = "grey80"))
+  
 
 # adres_ip
 
@@ -37,9 +84,3 @@ logs_nazwa <- logs_anonim %>%
 logs_nazwa_ip <- logs_anonim %>% 
   count(nazwa_anonim, adres_ip) %>% 
   count(nazwa_anonim)
-
-# zasób
-
-logs_kontekst <- logs_anonim %>% 
-  count(kontekst_zdarzenia) %>% 
-  arrange(desc(n))
--- 03_analiza_czasu.R
+++ 03_analiza_czasu.R
@@ -1,25 +1,40 @@
 library(tidyverse)
+library(lubridate)
 
 load("data/logs_anonim.RData")
 
 # tydzień przed pandemią
 
 logs_anonim %>% 
-  filter(godz >= "2020-03-09 01:00:00", godz <= "2020-03-15 23:00:00", nazwa_anonim != "ŁW") %>% 
+  filter(czas >= "2020-03-09 00:00:00", czas <= "2020-03-15 23:59:00", nazwa_anonim != "ŁW") %>% 
   count(czas) %>% 
-  ggplot(aes(x=czas, y=n)) + geom_point()
+  ggplot(aes(x=czas, y=n)) + 
+  geom_point(color = "#6daaee") +
+  ylab("Liczba akcji") +
+  ylim(0,50) +
+  ggtitle("Składniki kursu wg liczby wyświetleń - powyżej 1000") +
+  labs(caption = "Łukasz Wawrowski - wawrowski.edu.pl") +
+  theme_light() +
+  theme(plot.caption = element_text(color = "grey80"))
   
 # tydzień w pandemi
 
 logs_anonim %>% 
   filter(godz >= "2020-04-20 01:00:00", godz <= "2020-04-26 23:00:00", nazwa_anonim != "ŁW") %>% 
   count(czas) %>% 
-  ggplot(aes(x=czas, y=n)) + geom_point()
+  ggplot(aes(x=czas, y=n)) +
+  geom_point(color = "#6daaee") +
+  ylab("Liczba akcji") +
+  ylim(0,20) +
+  ggtitle("Składniki kursu wg liczby wyświetleń - powyżej 1000") +
+  labs(caption = "Łukasz Wawrowski - wawrowski.edu.pl") +
+  theme_light() +
+  theme(plot.caption = element_text(color = "grey80"))
 
 # cały semestr
 
 logs_anonim %>% 
-  filter(godz >= "2020-03-09 01:00:00", godz <= "2020-06-23 23:00:00", nazwa_anonim != "ŁW") %>% 
+  filter(godz >= "2020-03-09 01:00:00", godz <= "2020-06-14 23:00:00", nazwa_anonim != "ŁW") %>% 
   count(czas) %>% 
   ggplot(aes(x=czas, y=n)) + geom_point()
 
@@ -29,5 +44,24 @@ logs_anonim %>%
   filter(godz >= "2020-03-09 01:00:00", godz <= "2020-06-23 23:00:00", nazwa_anonim == "23JA9vQNpe") %>% 
   count(czas) %>% 
   ggplot(aes(x=czas, y=n)) + geom_point()
+
+# moje aktywności
+
+akt_dzien <- logs_anonim %>% 
+  filter(nazwa_anonim == "ŁW", godz >= "2020-03-09 01:00:00") %>% 
+  mutate(dzien=ymd(format(round(czas, units="days"), format="%Y-%m-%d"))) %>% 
+  count(dzien)
+
+ggTimeSeries::ggplot_calendar_heatmap(dtDateValue = akt_dzien, cDateColumnName = "dzien", cValueColumnName = "n") +
+  scale_fill_continuous(low = "#ffffbf", high = "#d73027") 
   
+# studenci
+
+akt_dzien <- logs_anonim %>% 
+  filter(nazwa_anonim != "ŁW", czas >= "2020-03-09 00:00:00") %>% 
+  mutate(dzien=ymd(format(round(czas, units="days"), format="%Y-%m-%d"))) %>% 
+  count(dzien)
+
+ggTimeSeries::ggplot_calendar_heatmap(dtDateValue = akt_dzien, cDateColumnName = "dzien", cValueColumnName = "n") +
+  scale_fill_continuous(low = "#ffffbf", high = "#d73027") 
 
